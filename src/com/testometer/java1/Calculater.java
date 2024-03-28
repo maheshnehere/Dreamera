e8ea96fb6d3dc4e9e3a2edcdffa9077014e0b612
maheshnehere <maheshnehere93@gmail.com>
2024-03-29 00:31:01 +0530

    Scanner class created in Calculater.java program

---
+++ src/com/testometer/java1/Calculater.java
@@ -0,0 +1,28 @@
+package com.testometer.java1;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Calculater {
+
+
+    public static void main(String[] args) {
+
+//        Calculater c = new Calculater();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select the calculation type to be performed");
+        String value = sc.next();
+
+        System.out.println("Enter number 1");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter number 2");
+        int num2 = sc.nextInt();
+
+
+
+
+    }
+}
