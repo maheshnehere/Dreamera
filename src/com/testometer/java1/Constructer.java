c5b56937ab94ad161a73c559117b9518f231bc99
maheshnehere <maheshnehere93@gmail.com>
2024-03-31 23:44:10 +0530

    constructor

---
+++ src/com/testometer/java1/Constructer.java
@@ -0,0 +1,17 @@
+package com.testometer.java1;
+
+public class Constructer {
+
+    int number;
+
+    int number2;
+
+    public Constructer(int num,int num1)
+    {
+        this.number  
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
